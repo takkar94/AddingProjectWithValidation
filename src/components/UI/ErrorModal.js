a99b5aab8aed9d1d43d21bef22fafd2d8ea28544
Aaditya Ghimire <ghimireaditya@gmail.com>
2022-07-03 17:35:32 +0545

    implemented css

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -4,14 +4,14 @@ import classes from "./ErrorModal.module.css"
 
 const ErrorModal = (props) => {
     return (
-        <Card>
-            <header>
+        <Card className={classes.modal}>
+            <header className={classes.header}>
                 <h2>{props.title}</h2>
             </header>
-            <div>
+            <div className={classes.content}>
                 <p>{props.message}</p>
             </div>
-            <footer>
+            <footer className={classes.actions}>
                 <Button>Okay</Button>
             </footer>
         </Card>
