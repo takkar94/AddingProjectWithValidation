4be9e7eb19487edce5d2a3f6c08f3b5933efafda
Aaditya Ghimire <ghimireaditya@gmail.com>
2022-07-03 11:12:22 +0545

    dynamic data entry

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,4 +1,4 @@
-import classes from "Button.module.css";
+import classes from "./Button.module.css";
 
 const Button = (props) => {
   return (
--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,19 +1,32 @@
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUsers.module.css";
+import { useState } from "react";
 
 const AddUser = (props) => {
+    
+    const [userName, setUserName] =useState('');
+    const [age, setAge] =useState('');
+    
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(userName, age)
     }
     
+    const usernameChangeHandler = (event) => {
+        setUserName(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setAge(event.target.value);
+    };
     return (
         <Card className={classes.input} >
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text"/>
+                <input id="username" type="text" onChange={usernameChangeHandler}/>
                 <label htmlFor="age">Age(Years)</label>
-                <input id="age" type="number"/>
+                <input id="age" type="number" onChange={ageChangeHandler}/>
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
